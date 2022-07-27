3d64a6b4683682549c0dd31f93016ad6a7db6154
Creedo11 <keegkree@gmail.com>
2022-07-26 20:40:26 -0400

    Somehow There Was a Project Folder Inside a Project Folder So Moved All Files to One Proejct Folder and Deleted the Other

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vpigxj+d%v!0gp63vf#1^l(w%_vaqcz42#%sg5!o66)t)e=gyp'
+SECRET_KEY = 'django-insecure-!bvvuxm38dsw$@+6ab)b3re0!_gkpoz-_0$vxf(asitj80lm0g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
