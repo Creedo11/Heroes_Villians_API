33d13ac09efffcdb03bbeeba17ff074812aba206
Creedo11 <keegkree@gmail.com>
2022-07-27 22:55:24 -0400

    Added Logic to Supers Admin File

--- supers/views.py
+++ supers/views.py
@@ -4,12 +4,20 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
+from supers import serializers
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-
-    return Response('')
+    if request.method == 'GET':
+        queryset = Super.objects.all()
+        serializer = SuperSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET'])
