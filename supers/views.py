270ea167638d2af27cef2ed2031d25894fcd2ade
Creedo11 <keegkree@gmail.com>
2022-07-27 21:15:48 -0400

    Updated Imports on Project URLs File + Began Logic on Supers Views File

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
 # Create your views here.
+@api_view(['GET'])
+def supers_list(request):
+
+    return Response('')
+
+
+@api_view(['GET'])
+def supers_detail(request):
+
+    return Response('')
+
